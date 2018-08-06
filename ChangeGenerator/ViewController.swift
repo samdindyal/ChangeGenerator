f6cc2dd537b79a66815a4500c2982b0787fa4dd7
Sam Dindyal <sam.dindyal@me.com>
2018-08-06 11:35:22 -0400

    Initial Commit

---
+++ ChangeGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChangeGenerator
+//
+//  Created by Sam Dindyal on 2018-08-06.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
