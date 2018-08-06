b7f6a9b7ba563d58462eec7e8068dac23e658bb0
Sam Dindyal <sam.dindyal@me.com>
2018-08-06 14:27:45 -0400

    Adds custom cell for displaying coins.

---
+++ ChangeGenerator/ChangeGeneratorController.swift
@@ -0,0 +1,13 @@
+//
+//  ChangeGeneratorController.swift
+//  ChangeGenerator
+//
+//  Created by Sam Dindyal on 2018-08-06.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class ChangeGeneratorController: UITableViewController {
+    @IBOutlet var amountLabel:UITextField!
+}
---
+++ ChangeGenerator/CoinCell.swift
@@ -0,0 +1,16 @@
+//
+//  CoinCell.swift
+//  ChangeGenerator
+//
+//  Created by Sam Dindyal on 2018-08-06.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class CoinCell: UITableViewCell {
+    @IBOutlet var coinLabel:UILabel!
+    @IBOutlet var valueLabel:UILabel!
+    @IBOutlet var amountLabel:UILabel!
+    
+}
