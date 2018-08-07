5597296c429a1152623d72a53010b7cf8c58d179
Sam Dindyal <sam.dindyal@me.com>
2018-08-07 11:10:55 -0400

    Creates Coin object.

---
+++ ChangeGenerator/Coin.swift
@@ -0,0 +1,19 @@
+//
+//  Coin.swift
+//  ChangeGenerator
+//
+//  Created by Sam Dindyal on 2018-08-07.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class Coin {
+    var name:String
+    var value:Float
+    
+    init(name: String, value: Float) {
+        self.name   = name
+        self.value  = value
+    }
+}
