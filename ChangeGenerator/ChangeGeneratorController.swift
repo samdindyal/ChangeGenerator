cde4171406920f04edb8f93c9adc2cff7bd98513
Sam Dindyal <sam.dindyal@me.com>
2018-08-07 14:51:58 -0400

    Sets cells with coin values

--- ChangeGenerator/ChangeGeneratorController.swift
+++ ChangeGenerator/ChangeGeneratorController.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class ChangeGeneratorController: UITableViewController {
     @IBOutlet var amountLabel:UITextField!
+    
+    var coins:[Coin]! {
+        didSet {
+            self.coins.sort { (a, b) -> Bool in
+                a.value > b.value
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coins.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoinCell", for: indexPath) as! CoinCell
+        
+        let row = indexPath.row
+        let coin = self.coins[row]
+        cell.coinLabel.text = "\(coin.name)"
+        cell.valueLabel.text = "$\(coin.value)"
+        cell.amountLabel.text = "x?"
+        
+        
+        return cell
+    }
 }
